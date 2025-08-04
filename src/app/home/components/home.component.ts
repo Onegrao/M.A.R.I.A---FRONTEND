4ca286fef27dc3f29c34391d7ebdfe36b3130129
Lais Carvalho Pantoja <lais.carvalho@ti.local>
2025-08-04 16:48:08 -0400

    adicionado icons no home

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  popupVisible = false;
+
+  showPopup() {
+    this.popupVisible = true;
+  }
+
+  hidePopup() {
+    this.popupVisible = false;
+  }
+
+  saveMachine() {
+    const name = (document.getElementById('machineName') as HTMLInputElement).value;
+    const type = (document.getElementById('machineType') as HTMLInputElement).value;
+    console.log('Machine saved:', { name, type });
+    this.hidePopup();
+  }
+}
