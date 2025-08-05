de393b58774b3ab83c873faee79ad015528b46c8
Lais Carvalho Pantoja <lais.carvalho@ti.local>
2025-08-05 16:48:35 -0400

    algumas modificações do home

--- src/app/home/components/home.component.ts
+++ src/app/home/components/home.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   popupVisible = false;
+  sidebarOpen = false;
 
   showPopup() {
     this.popupVisible = true;
@@ -15,6 +18,10 @@ export class HomeComponent {
 
   hidePopup() {
     this.popupVisible = false;
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
   }
 
   saveMachine() {
